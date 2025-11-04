4cec9de788bc267785add23cb09477b2404c43d0
Lương Thị Phượng <yirpng@gmail.com>
2025-11-04 23:50:03 +0700

    edit header

--- app/gui/components/note_detail.py
+++ app/gui/components/note_detail.py
@@ -9,7 +9,15 @@ class NoteDetail(ctk.CTkFrame):
         self.current_note_id = None
         self.parent = parent  # Thêm để refresh list
         
-       
+        # Header
+        header = ctk.CTkFrame(self, fg_color="transparent")
+        header.pack(fill="x", padx=20, pady=20)
+        
+        ctk.CTkLabel(
+            header,
+            text="Chi tiết ghi chú",
+            font=("Segoe UI", 18, "bold")
+        ).pack(side="left")
         
         # Nút tạo mới
         ctk.CTkButton(
@@ -30,15 +38,6 @@ class NoteDetail(ctk.CTkFrame):
         )
         self.title_entry.pack(fill="x", padx=20, pady=(0, 10))
         
-         # Header
-        header = ctk.CTkFrame(self, fg_color="transparent")
-        header.pack(fill="x", padx=20, pady=20)
-        
-        ctk.CTkLabel(
-            header,
-            text="Chi tiết ghi chú",
-            font=("Segoe UI", 18, "bold")
-        ).pack(side="left")
         
         # Content input
         self.content_text = ctk.CTkTextbox(
